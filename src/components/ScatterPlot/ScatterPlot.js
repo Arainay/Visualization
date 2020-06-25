85bd99a648677ccfb11f207d387c02a94a5a1155
Sergey Lobanov <arainay@gmail.com>
2020-06-25 21:47:41 +0800

    Added dataset

--- src/components/ScatterPlot/ScatterPlot.js
+++ src/components/ScatterPlot/ScatterPlot.js
@@ -1,5 +1,8 @@
 import React, { useRef } from 'react';
 import Svg from '@app/components/Svg';
+import dataset from './data/auto-mpg.csv';
+
+console.log(dataset);
 
 const ScatterPlot = () => {
   const svgRef = useRef(null);

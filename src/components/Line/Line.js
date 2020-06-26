0d3a329e1bcc4bbab81992a160e35218f0d135c8
Sergey Lobanov <arainay@gmail.com>
2020-06-26 19:58:23 +0800

    Added path

--- src/components/Line/Line.js
+++ src/components/Line/Line.js
@@ -2,6 +2,9 @@ import React, { useEffect, useRef } from 'react';
 import { extent } from 'd3-array';
 import { format } from 'd3-format';
 import { scaleLinear, scaleTime } from 'd3-scale';
+import { line } from 'd3-shape';
+import { transition } from 'd3-transition';
+import { easeLinear } from 'd3-ease';
 import Svg from '@app/components/Svg';
 import dataset from './data/temperature_Moscow.csv';
 import { select } from 'd3-selection';
@@ -9,12 +12,15 @@ import { axisBottom, axisLeft } from 'd3-axis';
 
 const Line = () => {
   const svgRef = useRef(null);
+  const pathRef = useRef(null);
 
-  const data = dataset.map(item => ({
-    city: item.City,
-    date: new Date(item.Year, item.Month - 1, item.Day),
-    temperature: format(".3n")((item.AvgTemperature - 32) / 1.8)
-  }));
+  const data = dataset
+    .map((item) => ({
+      city: item.City,
+      date: new Date(item.Year, item.Month - 1, item.Day),
+      temperature: Number(format(".3n")((item.AvgTemperature - 32) / 1.8))
+    }))
+    .filter(({ temperature }) => temperature !== -72.8);
 
   const margin = {
     top: 20,
@@ -41,6 +47,25 @@ const Line = () => {
 
     const xAxis = axisBottom(xScale);
     const yAxis = axisLeft(yScale);
+    const lineGenerator = line()
+      .x(d => xScale(d.date))
+      .y(d => yScale(d.temperature));
+
+    const path = select(pathRef.current);
+
+    path
+      .attr('d', lineGenerator(data));
+
+    const pathLength = path.node().getTotalLength();
+    const pathTransition = transition()
+      .duration(5000)
+      .ease(easeLinear);
+
+    path
+      .attr('stroke-dashoffset', pathLength)
+      .attr("stroke-dasharray", pathLength)
+      .transition(pathTransition)
+        .attr('stroke-dashoffset', 0);
 
     g.append('g')
       .attr('transform', `translate(0, ${innerHeight})`)
@@ -48,11 +73,27 @@ const Line = () => {
 
     g.append('g')
       .call(yAxis);
+
+    g.append('g')
+      .call(
+        axisLeft(yScale)
+          .ticks(yScale.ticks().length)
+          .tickSize(-width + margin.left + margin.right)
+          .tickFormat('')
+      )
+      .call(d =>
+        d.selectAll(".tick line")
+          .attr("stroke-opacity", 0.5)
+          .attr("stroke-dasharray", 1))
+      .select('.domain')
+      .remove();
   }, []);
 
   return (
     <Svg ref={svgRef}>
-      <g id="data" transform={`translate(${margin.left}, ${margin.top})`}/>
+      <g id="data" transform={`translate(${margin.left}, ${margin.top})`}>
+        <path ref={pathRef} d={''} stroke="lightblue" fill="none"/>
+      </g>
     </Svg>
   );
 };

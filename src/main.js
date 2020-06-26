1eaff1ae9e215e7376d5e25be262d871ca3a43d5
Sergey Lobanov <arainay@gmail.com>
2020-06-26 16:14:45 +0800

    Deleted Suspense usage

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Visualization from '@app/components/Visualization';
 
 render(
-  <Suspense fallback={<div>Loading...</div>}>
-    <BrowserRouter>
-      <Visualization/>
-    </BrowserRouter>
-  </Suspense>,
+  <BrowserRouter>
+    <Visualization/>
+  </BrowserRouter>,
   document.getElementById('root')
 );
